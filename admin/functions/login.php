75689ded19a3cc457937d44d886cb0627521a25b
Nyhx1213 <matis1013@gmail.com>
2026-03-31 09:03:49 +0200

    Ajout d'animation CSS pour le formulaire de connexion admin
    Modification de la langue html à fr
    
    Suppression de truc.php

--- admin/functions/login.php
+++ admin/functions/login.php
@@ -6,20 +6,20 @@ require_once "utilities.php";
 
 if ($_SERVER['REQUEST_METHOD'] !== "POST") {
     $_SESSION['popup'] = "Veuillez acceder à travers un formulaire à la page.";
-    header("Location: index.php"); // Ajouter page d'erreur. 
+    header("Location: ../views/index.php"); // Ajouter page d'erreur. 
     exit();
 }
 
 if (!isset($_POST['username']) || !isset($_POST['password'])) {
     $_SESSION['popup'] = "Veuillez remplir les données du formulaire";
-    header("Location: index.php"); // Page de login.
+    header("Location: ../views/index.php"); // Page de login.
     exit();
 }
 
 if (!validateUsername($_POST['username'], 4, 20) || !validatePassword($_POST['password'], 8, 24)) {
     $_SESSION['popup'] = "Mot de passe ou utilisateur non conforme";
     echo "2";
-    header("Location: index.php"); //  Page de login.
+    header("Location: ../views/index.php"); //  Page de login.
     exit();
 }
 
@@ -34,7 +34,7 @@ try {
     $statement->execute();
 } catch (Exception $e) {
     echo $e->getMessage();
-    header("Location: error.php");
+    header("Location: ../views/error.php");
     exit();
 }
 
@@ -42,7 +42,7 @@ if ($row = $statement->fetch()) {
     $password_hash = $row['password'];
 } else {
     $_SESSION['popup'] = 'Utilisateur non trouvée';
-    header("Location: index.php");
+    header("Location: ../views/index.php");
     exit();
 }
 $statement->closeCursor();
@@ -53,7 +53,7 @@ if (password_verify($password, $password_hash)) {
         'username' => $username
     );
     $_SESSION['popup'] = "Identifiants bon";
-    header("Location: dashboard.php");
+    header("Location: ../views/dashboard.php");
     exit();
 } else {
     $_SESSION['popup'] = "Identifiants mauvais";
--- admin/functions/truc.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo password_hash("Ucawb61upJQEdO", PASSWORD_DEFAULT);
-?>
